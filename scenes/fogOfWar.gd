81d603a01b4b44bfbba72cb24625f39f5c997725
Feralbear <68873979+Feralbear@users.noreply.github.com>
2022-12-15 14:47:01 +0100

    DSS_ver_dev

---
+++ scenes/fogOfWar.gd
@@ -0,0 +1,21 @@
+extends TextureRect
+
+func _ready():
+	pass
+
+func _process(delta):
+	if Game.character.path.size() == 0:
+		$Tween.interpolate_property(
+			self, "self_modulate:a",
+			self_modulate.a, 0.0, 1.0,
+			Tween.TRANS_SINE, Tween.EASE_IN
+		)
+		$Tween.start()
+	
+	else:
+		$Tween.interpolate_property(
+			self, "self_modulate:a",
+			self_modulate, 1.0, 1.0,
+			Tween.TRANS_SINE, Tween.EASE_IN
+		)
+		$Tween.start()
